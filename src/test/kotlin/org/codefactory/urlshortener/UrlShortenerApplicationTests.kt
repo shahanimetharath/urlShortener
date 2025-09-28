f57339948db51483d2206196ea26d27254c9edbc
shahanimetharath <ayshashahani15@gmail.com>
2025-09-28 21:43:58 +0200

    test case 1 : shorten URL should return a short code

--- src/test/kotlin/org/codefactory/urlshortener/UrlShortenerApplicationTests.kt
+++ src/test/kotlin/org/codefactory/urlshortener/UrlShortenerApplicationTests.kt
@@ -1,13 +1,37 @@
 package org.codefactory.urlshortener
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.codefactory.urlshortener.exception.InvalidUrlException
+import org.codefactory.urlshortener.exception.UrlNotFoundException
+import org.codefactory.urlshortener.service.UrlShortenerService
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 @SpringBootTest
-class UrlShortenerApplicationTests {
+@AutoConfigureMockMvc
+class UrlShortenerApplicationTests(
+    @Autowired val service: UrlShortenerService,
+    @Autowired val mockMvc: MockMvc,
+    @Autowired private val jacksonObjectMapper: ObjectMapper
+) {
 
     @Test
-    fun contextLoads() {
+    fun `shorten URL should return a short code`() {
+        val originalUrl = "https://www.google.com/"
+        val shortCode = service.shortenUrl(originalUrl)
+
+        assertNotNull(shortCode)
+        if (shortCode != null) {
+            assertTrue(shortCode.length in 4..10)
+        }
     }
+
 
 }
